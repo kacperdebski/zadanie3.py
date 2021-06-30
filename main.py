10b40047f6647dfb7fae57895b574824caeb6080
kacperdebski <79713034+kacperdebski@users.noreply.github.com>
2021-06-30 13:16:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from abc import ABC, abstractmethod
 import math
-def z1a(liczba: int) -> bool:
-    return liczba % 3 == 0
-def z1b(liczba: int) -> bool:
-    return int(math.sqrt(liczba)) ** 2 == liczba
+
+class Figura(ABC):
+    @abstractmethod
+    def nazwa(self):
+        pass
+
+class Kwadrat(Figura):
+    def __init__(self, bok1):
+        self.bok1 = bok1
+
+    def oblicz_pole(self):
+        return self.bok1 ** 2
+
+class Prostokat(Kwadrat):
+    def __init__(self, bok1, bok2):
+        super().__init__(bok1)
+        self.bok2 = bok2
+
+    def oblicz_pole(self):
+        return self.bok1 * self.bok2
